66faf849975537608d60bca0adc1d7e9e1af5d82
Minimum <s5550055@yahoo.com.tw>
2016-03-14 05:35:13 +0000

    Fix compiling error

--- openfalcon-build.sh
+++ openfalcon-build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+export REMOTE=origin
+export BRANCH=master
+export WORKDIR=Cepave
+export GITHUB=$GOPATH/src/github.com
+export WORKPATH=$GITHUB/$WORKDIR
+
 #######################################
 # Download Source Code
 #######################################
-rm -fR $GOPATH/src
-mkdir -p $GOPATH/src/github.com
-cd $GOPATH/src/github.com
-git clone --quiet -b master https://github.com/Cepave/open-falcon.git open-falcon
-cd open-falcon
-git submodule --quiet update --init --recursive
-git submodule --quiet foreach --recursive git checkout -f origin/master
+rm -fR $GITHUB
+mkdir -p $WORKPATH
+git clone --quiet -b master https://github.com/Cepave/open-falcon.git $WORKPATH
+cd $WORKPATH && git submodule update --quiet --init --recursive
 
 #######################################
 # Parse Arguments
@@ -17,69 +20,69 @@ git submodule --quiet foreach --recursive git checkout -f origin/master
 for arg in "$@" ; do
   case $arg in
   "agent")
-    BUILD_AGENT=true
+    export BUILD_AGENT=true
     ;;
   "graph")
-    BUILD_GRAPH=true
+    export BUILD_GRAPH=true
     ;;
   "query")
-    BUILD_QUERY=true
+    export BUILD_QUERY=true
     ;;
   "dashboard")
-    BUILD_DASHBOARD=true
+    export BUILD_DASHBOARD=true
     ;;
   "sender")
-    BUILD_SENDER=true
+    export BUILD_SENDER=true
     ;;
   "link")
-    BUILD_LINKS=true
+    export BUILD_LINKS=true
     ;;
   "links")
-    BUILD_LINKS=true
+    export BUILD_LINKS=true
     ;;
   "portal")
-    BUILD_PORTAL=true
+    export BUILD_PORTAL=true
     ;;
   "hbs")
-    BUILD_HBS=true
+    export BUILD_HBS=true
     ;;
   "alarm")
-    BUILD_ALARM=true
+    export BUILD_ALARM=true
     ;;
   "fe")
-    BUILD_FE=true
+    export BUILD_FE=true
     ;;
   "judge")
-    BUILD_JUDGE=true
+    export BUILD_JUDGE=true
     ;;
   "transfer")
-    BUILD_TRANSFER=true
+    export BUILD_TRANSFER=true
     ;;
   "task")
-    BUILD_TASK=true
+    export BUILD_TASK=true
     ;;
   "nodata")
-    BUILD_NODATA=true
+    export BUILD_NODATA=true
     ;;
   "aggregator")
-    BUILD_AGGREGATOR=true
+    export BUILD_AGGREGATOR=true
     ;;
   "all")
-    BUILD_AGENT=true
-    BUILD_GRAPH=true
-    BUILD_QUERY=true
-    BUILD_DASHBOARD=true
-    BUILD_SENDER=true
-    BUILD_LINKS=true
-    BUILD_PORTAL=true
-    BUILD_HBS=true
-    BUILD_ALARM=true
-    BUILD_FE=true
-    BUILD_JUDGE=true
-    BUILD_TRANSFER=true
-    BUILD_TASK=true
-    BUILD_NODATA=true
-    BUILD_AGGREGATOR=true
+    export BUILD_AGENT=true
+    export BUILD_GRAPH=true
+    export BUILD_QUERY=true
+    export BUILD_DASHBOARD=true
+    export BUILD_SENDER=true
+    export BUILD_LINKS=true
+    export BUILD_PORTAL=true
+    export BUILD_HBS=true
+    export BUILD_ALARM=true
+    export BUILD_FE=true
+    export BUILD_JUDGE=true
+    export BUILD_TRANSFER=true
+    export BUILD_TASK=true
+    export BUILD_NODATA=true
+    export BUILD_AGGREGATOR=true
     ;;
   *)
     echo -e "Specify one component to build:\nagent\ngraph\nquery\ndashboard\nsender\nlinks\nportal\nhbs\nalarm\nfe\njudge\ntransfer\ntask\nnodata\naggregator"
@@ -92,196 +95,106 @@ done
 #######################################
 # Build, Package, Rename and Collect
 #######################################
-PACKDIR=/package
-LOGFILE=$PACKDIR/filelist.txt
+export PACKDIR=/package
+export LOGFILE=$PACKDIR/filelist.txt
 rm -fR $PACKDIR
 mkdir -p $PACKDIR
 
-# Agent
-if [ ! -z $BUILD_AGENT ] && $BUILD_AGENT ; then
-  cd $GOPATH/src/github.com/open-falcon/agent
+function _build() {
+  cd $WORKPATH/$1
+  git checkout -f $REMOTE/$BRANCH
   go get ./...
-  ./control build
+  if [ "$2" = "do_build" ] ; then
+    ./control build
+  fi
   ./control pack
-  PACKFILE=$(find -name "*agent*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
+  export PACKFILE=$(find -name "*$1*.gz")
+  export NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.[:alnum:]]*\.tar.gz/.tar.gz/')"
   mv $PACKFILE $PACKDIR/$NEWFILE && \
     echo "Success... $NEWFILE" && \
     echo $PACKFILE >> $LOGFILE
+}
+
+function build() {
+  _build $1 do_build
+}
+
+function pack() {
+  _build $1 do_pack
+}
+
+# Agent
+if [ ! -z $BUILD_AGENT ] && $BUILD_AGENT ; then
+  build agent
 fi
 
 # Graph
 if [ ! -z $BUILD_GRAPH ] && $BUILD_GRAPH ; then
-  cd $GOPATH/src/github.com/open-falcon/graph
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*graph*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build graph
 fi
 
 # Query
 if [ ! -z $BUILD_QUERY ] && $BUILD_QUERY ; then
-  cd $GOPATH/src/github.com/open-falcon/query
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*query*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build query
 fi
 
 # Dashboard
 if [ ! -z $BUILD_DASHBOARD ] && $BUILD_DASHBOARD ; then
-  cd $GOPATH/src/github.com/open-falcon/dashboard
-  ./control pack
-  PACKFILE=$(find -name "*dashboard*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[[:alnum:]]*\.tar\.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  pack dashboard
 fi
 
 # Sender
 if [ ! -z $BUILD_SENDER ] && $BUILD_SENDER ; then
-  cd $GOPATH/src/github.com/open-falcon/sender
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*sender*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build sender
 fi
 
 # Links
 if [ ! -z $BUILD_LINKS ] && $BUILD_LINKS ; then
-  cd $GOPATH/src/github.com/open-falcon/links
-  ./control pack
-  PACKFILE=$(find -name "*link*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[[:alnum:]]*\.tar\.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  pack links
 fi
 
 # Portal
 if [ ! -z $BUILD_PORTAL ] && $BUILD_PORTAL ; then
-  cd $GOPATH/src/github.com/open-falcon/portal
-  ./control pack
-  PACKFILE=$(find -name "*portal*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[[:alnum:]]*\.tar\.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  pack portal
 fi
 
 # Heartbeat Server (HBS)
 if [ ! -z $BUILD_HBS ] && $BUILD_HBS ; then
-  cd $GOPATH/src/github.com/open-falcon/hbs
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*hbs*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build hbs
 fi
 
 # Alarm
 if [ ! -z $BUILD_ALARM ] && $BUILD_ALARM ; then
-  cd $GOPATH/src/github.com/open-falcon/alarm
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*alarm*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build alarm
 fi
 
 # FE
 if [ ! -z $BUILD_FE ] && $BUILD_FE ; then
-  cd $GOPATH/src/github.com/open-falcon/fe
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*fe*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build fe
 fi
 
 # Judge
 if [ ! -z $BUILD_JUDGE ] && $BUILD_JUDGE ; then
-  cd $GOPATH/src/github.com/open-falcon/judge
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*judge*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build judge
 fi
 
 # Transfer
 if [ ! -z $BUILD_TRANSFER ] && $BUILD_TRANSFER ; then
-  cd $GOPATH/src/github.com/open-falcon/transfer
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*transfer*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build transfer
 fi
 
 # Task
 if [ ! -z $BUILD_TASK ] && $BUILD_TASK ; then
-  cd $GOPATH/src/github.com/open-falcon/task
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*task*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build task
 fi
 
 # Nodata
 if [ ! -z $BUILD_NODATA ] && $BUILD_NODATA ; then
-  cd $GOPATH/src/github.com/open-falcon/nodata
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*nodata*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build nodata
 fi
 
 # Aggregator
 if [ ! -z $BUILD_AGGREGATOR ] && $BUILD_AGGREGATOR ; then
-  cd $GOPATH/src/github.com/open-falcon/aggregator
-  go get ./...
-  ./control build
-  ./control pack
-  PACKFILE=$(find -name "*aggregator*.gz")
-  NEWFILE="$(echo ${PACKFILE##*/} | sed -e 's/-[.0-9]*\.tar.gz/.tar.gz/')"
-  mv $PACKFILE $PACKDIR/$NEWFILE && \
-    echo "Success... $NEWFILE" && \
-    echo $PACKFILE >> $LOGFILE
+  build aggregator
 fi
+
